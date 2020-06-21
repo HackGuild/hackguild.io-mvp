5eb35e411db84c24b910dc78f9e5cfa1f7bf6fa9
Alex <afriedman1991@gmail.com>
2020-06-21 16:45:13 -0400

    fixed index.js so i can deploy this thing.

--- hackguild/src/pages/index.js
+++ hackguild/src/pages/index.js
@@ -1,21 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import { Layout } from "../components/organisms"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>YEET yourselves out of this broken page and go to the homepage demo page instead.</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/demo/">Mission demo</Link> <br />
+    <Link to="/home/">HOMEPAGE demo</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
