1df344ec95f8bee44d574d08cd6341245536a39c
Kinnera Banda <kinnerabanda@gmail.com>
2020-06-30 15:38:23 -0400

    changes to modal. used react-modal.

--- hackguild/src/components/organisms/Team/Team.jsx
+++ hackguild/src/components/organisms/Team/Team.jsx
@@ -3,16 +3,36 @@ import { BioCard } from "../../molecules"
 import IA from "../../../images/headshot.png"
 
 export default function Team() {
-  return(
+  return (
     <div className="Team">
       <div className="team-title">
-          <h1>HackGuild Team</h1>
+        <h1>HackGuild Team</h1>
       </div>
-      <BioCard headshot={IA} name="Isabel Abonitalla" position="Co-founder, Executive Director" />
-      <BioCard headshot="https://source.unsplash.com/random/150x150" name="Michael Cao" position="Co-founder, Executive Director" />
-      <BioCard headshot="https://source.unsplash.com/random/150x150" name="Lily Gong" position="Content Creator" />
-      <BioCard headshot="https://source.unsplash.com/random/150x150" name="Kristen Lee" position="Operations Intern" />
-      <BioCard headshot="https://source.unsplash.com/random/150x150" name="Estrella Popoca" position="Marketing Specialist" />
+      <BioCard
+        headshot={IA}
+        name="Isabel Abonitalla"
+        position="Co-founder, Executive Director"
+      />
+      <BioCard
+        headshot="https://source.unsplash.com/random/150x150"
+        name="Michael Cao"
+        position="Co-founder, Executive Director"
+      />
+      <BioCard
+        headshot="https://source.unsplash.com/random/150x150"
+        name="Lily Gong"
+        position="Content Creator"
+      />
+      <BioCard
+        headshot="https://source.unsplash.com/random/150x150"
+        name="Kristen Lee"
+        position="Operations Intern"
+      />
+      <BioCard
+        headshot="https://source.unsplash.com/random/150x150"
+        name="Estrella Popoca"
+        position="Marketing Specialist"
+      />
     </div>
-  );
+  )
 }
