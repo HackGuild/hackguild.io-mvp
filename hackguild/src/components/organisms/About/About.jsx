b2ff9b820d7a83407aa565bd9b03dade5cf0e271
lilyygong <37991602+lilyygong@users.noreply.github.com>
2020-06-30 00:52:08 -0700

    trying to fix about page

--- hackguild/src/components/organisms/About/About.jsx
+++ hackguild/src/components/organisms/About/About.jsx
@@ -4,7 +4,7 @@ import './About.scss'
 export default function About() {
   return (
     <div className="About">
-      <h1 className="Heading">HackGuild offers:</h1>
+      <h1 className="Title">HackGuild offers:</h1>
       <p className="Bullet">＞ mentorship for hosting tech events</p>
       <p className="Bullet">＞ access to resources for organizing events</p>
       <p className="Bullet">＞ a network of innovators, leaders, and technologists</p>
