a157b2c420bf84d81ad1c68a4ed8796a7cace933
Alex <afriedman1991@gmail.com>
2020-06-30 13:45:44 -0400

    metadata changes for title and authors

--- hackguild/gatsby-config.js
+++ hackguild/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `HackGuild`,
+    description: `A global nonprofit dedicated to expanding access to tech education to underserved communities and empowering the next generation of innovators, computer scientists, and tech professionals.`,
+    author: `Isabel Abonitalla, Kinnera Banda, Lily Gong, Anushka Saxena, Lauren Zaiden`,
   },
   plugins: [
     `gatsby-plugin-sass`,
