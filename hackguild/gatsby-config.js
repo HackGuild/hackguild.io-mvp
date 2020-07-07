cc5f635a025b690b2cefdfb69d690f3dcd255478
asaxena2019 <anushkasaxena2019@gmail.com>
2020-07-06 20:10:59 -0400

    fixed header and footer

--- hackguild/gatsby-config.js
+++ hackguild/gatsby-config.js
@@ -46,6 +46,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./relative/path/to/layout/component`),
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
