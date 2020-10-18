6a73b28f923fee9633decca88b987cb0e378e462
Blake Vente <venteralph@gmail.com>
2020-10-17 20:13:24 -0400

    changes on test page

--- src/components/organisms/SemanticUI/SemanticTest.jsx
+++ src/components/organisms/SemanticUI/SemanticTest.jsx
@@ -13,57 +13,63 @@ import {
 
 export default function SemanticTest() {
   return (
-    <Container fluid style={{padding: '8em'}}>
-      <Segment vertical >
-        <Grid  stackable verticalAlign="middle" columns='2'>
-          <Grid.Row>
-            <Grid.Column width={8}>
-              <Header as="h2" color="violet">
-                Our Mission
-              </Header>
-              <Header as="h3" style={{ fontSize: "2em" }}>
-                We Help Companies and Companions
-              </Header>
-              <p>
-                We can give your company superpowers to do things that they
-                never thought possible. Let us delight your customers and
-                empower your needs... through pure data analytics.
-              </p>
-              <Header as="h3" style={{ fontSize: "2em" }}>
-                We Make Bananas That Can Dance
-              </Header>
-              <p style={{ fontSize: "1.33em" }}>
-                Yes that's right, you thought it was the stuff of dreams, but
-                even bananas can be bioengineered.
-              </p>
-            </Grid.Column>
-            <Grid.Column floated="right" width={6}>
-              <Image
-                bordered
-                rounded
-                size="huge"
-                src="https://via.placeholder.com/150" />
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column textAlign="left">
-              <Button primary size="huge">Check Them Out</Button>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+    <>
+      
+      <Container fluid style={{ padding: "8em" }}>
+        <Segment vertical>
+          <Grid stackable verticalAlign="middle" columns="2">
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <Header as="h2" color="violet">
+                  Our Mission
+                </Header>
+                <Header as="h3" style={{ fontSize: "2em" }}>
+                  We Help Companies and Companions
+                </Header>
+                <p>
+                  We can give your company superpowers to do things that they
+                  never thought possible. Let us delight your customers and
+                  empower your needs... through pure data analytics.
+                </p>
+                <Header as="h3" style={{ fontSize: "2em" }}>
+                  We Make Bananas That Can Dance
+                </Header>
+                <p style={{ fontSize: "1.33em" }}>
+                  Yes that's right, you thought it was the stuff of dreams, but
+                  even bananas can be bioengineered.
+                </p>
+              </Grid.Column>
+              <Grid.Column floated="right" width={6}>
+                <Image
+                  bordered
+                  rounded
+                  size="huge"
+                  src="https://via.placeholder.com/150"
+                />
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column textAlign="left">
+                <Button primary size="huge">
+                  Check Them Out
+                </Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
 
-      {/* Design: page 2 Learn more about the members behind HackGuild */}
-      <Header as="h2" textAlign="center" color="violet">
-        Our Team
-        <Header.Subheader>
-          This page contains some helpful examples that can be usefull for
-          advanced layouts.
-        </Header.Subheader>
-      </Header>
+          {/* Design: page 2 Learn more about the members behind HackGuild */}
+          <Header as="h2" textAlign="center" color="violet">
+            Our Team
+            <Header.Subheader>
+              This page contains some helpful examples that can be usefull for
+              advanced layouts.
+            </Header.Subheader>
+          </Header>
 
-      <Button primary>Primary</Button>
-      <Button secondary>Secondary</Button>
-      </Segment>
-    </Container>
+          <Button primary>Primary</Button>
+          <Button secondary>Secondary</Button>
+        </Segment>
+      </Container>
+    </>
   )
 }
